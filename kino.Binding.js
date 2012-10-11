414209b3892b93e3ff4aaa5b525d3f96de256201
kinogam <kinogam@gmail.com>
2012-10-11 11:42:53 +0800

    add

---
+++ kino.Binding.js
@@ -0,0 +1,83 @@
+﻿(function (window) {
+    "use strict";
+
+    var exports = {};
+
+    exports.process = function (rootElement, dataContext) {
+        ///<summary>
+        /// process elements binding
+        ///</summary>
+        ///<param name="rootElement" type="Element">
+        /// element for bind
+        ///</param>
+        ///<param name="dataContext" type="Object">
+        /// binding data
+        ///</param>
+
+        var elements = rootElement.getElementsByTagName("*");
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (element.hasAttribute("data-bind")) {
+                bindAttr(element.getAttribute("data-bind"), element, dataContext);
+            }
+        }
+    };
+
+    exports.as = function (dataContext) {
+        ///<summary>
+        /// dynamic binding object
+        ///</summary>
+        ///<param name="dataContext" type="Object">
+        /// convert object
+        ///</param>
+        var bObj = {
+            _isBO: true,
+            bgData: dataContext,
+            _changeMap: {},
+            set: function (options) {
+                for (var i in options) {
+                    if (this.bgData[i] != options[i]) {
+                        this.changed(this.bgData, i, options[i]);
+                    }
+                }
+            },
+            changed: function (obj, property, value) {
+                obj[property] = value;
+                var mobj = this._changeMap[property];
+                if (mobj.changeHandle != undefined) {
+                    mobj.changeHandle(value);
+                }
+            }
+        };
+
+        for (var i in dataContext) {
+            bObj[i] = dataContext[i];
+            bObj._changeMap[i] = {};
+        }
+        return bObj;
+    }
+
+    var bindAttr = function (dataBindAttr, element, dataContext) {
+        var group;
+        var r = new RegExp("([^;:\\s]+)\\s*:\\s*([^\\s;]+)", "g");
+        while ((group = r.exec(dataBindAttr)) != null) {
+            var domProperty = group[1];
+            var objProperty = group[2];
+            element[domProperty] = dataContext[objProperty];
+
+            updateView(element, dataContext, domProperty, objProperty);
+        }
+    };
+
+    var updateView = function (element, dataContext, domProperty, objProperty) {
+        if (dataContext._isBO) {
+            dataContext._changeMap[objProperty].changeHandle = function (value) {
+                element[domProperty] = value;
+            }
+        }
+    };
+
+    window.kino || (window.kino = {});
+    window.kino.Binding = exports;
+
+})(window);
---
+++ test/kino.Binding.test.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="qunit/qunit.js" />
+/// <reference path="../kino.bind.js" />
+
+module("process");
+
+test("can process binding data", function () {
+    var dom = document.createElement("div");
+    dom.innerHTML = "<div data-bind='innerHTML: name'></div>";
+    kino.Binding.process(dom, { name: 'kino' });
+    equal(dom.getElementsByTagName("div")[0].innerHTML, "kino");
+});
+
+test("can process multi-attribute", function () {
+    var dom = document.createElement("div");
+    dom.innerHTML = "<div data-bind='innerHTML: name;title: title'></div>";
+    kino.Binding.process(dom, { name: 'kino', title: 'my title' });
+
+    var div = dom.getElementsByTagName("div")[0];
+    equal(div.innerHTML, "kino");
+    equal(div.getAttribute("title"), "my title");
+});
+
+module("as");
+
+test("change dataContext property should update view", function () {
+    var dom = document.createElement("div");
+    dom.innerHTML = "<div data-bind='innerHTML: name;title: title'></div>";
+
+    var bindingObj = kino.Binding.as({
+        name: 'kino',
+        title: 'my title'
+    });
+
+    kino.Binding.process(dom, bindingObj);
+
+    var div = dom.getElementsByTagName("div")[0];
+    equal(div.innerHTML, "kino");
+    equal(div.getAttribute("title"), "my title");
+
+    bindingObj.set({
+        title: "mago",
+        name: "changed name"
+    });
+
+    equal(div.innerHTML, "changed name");
+    equal(div.getAttribute("title"), "mago");
+});
